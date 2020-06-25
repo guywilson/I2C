6fc1a1a8ca26d058cc80e81e6c7bcfd8e2139d01
Guy Wilson <wilson.guy@gmail.com>
2020-06-25 18:41:02 +0100

    Added CPU compensation

--- src/test.cpp
+++ src/test.cpp
@@ -8,6 +8,35 @@
 
 using namespace std;
 
+#define TEMP_COMPENSATION_FACTOR            2.25
+
+double getCPUTemp()
+{
+    double      cpuTemp = -299.0;
+    FILE *      fptr;
+    char        szTemp[8];
+    int         i = 0;
+
+    fptr = fopen("/sys/class/thermal/thermal_zone0/temp", "rt");
+
+    if (fptr == NULL) {
+        printf("Could not open cpu temperature file");
+        exit(-1);
+    }
+
+    while (!feof(fptr)) {
+        szTemp[i++] = (char)fgetc(fptr);
+    }
+    
+    szTemp[i] = 0;
+
+    fclose(fptr);
+
+    cpuTemp = atof(szTemp) / 1000.0;
+
+    return cpuTemp;
+}
+
 int main(void)
 {
     BME280_TPH      tph;
@@ -22,7 +51,7 @@ int main(void)
 
     bme280.readTPH(&tph);
 
-    printf("Temperature: %.2f\n", tph.temperature);
+    printf("Temperature: %.2f\n", tph.temperature - ((getCPUTemp() - tph.temperature) / TEMP_COMPENSATION_FACTOR));
     printf("Pressure: %.2f\n", tph.pressure);
     printf("Humidity: %.2f\n", tph.humidity);
 
