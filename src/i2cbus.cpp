135f7e185627ba2e9d1ad91b550c330c96b413a2
Guy Wilson <wilson.guy@gmail.com>
2020-07-03 17:04:26 +0100

    Debug

--- src/i2cbus.cpp
+++ src/i2cbus.cpp
@@ -76,6 +76,8 @@ void I2CBus::busRead(void * data, uint32_t dataLength)
 
 void I2CBus::acquire(const char * deviceName)
 {
+    printf("Acquiring bus for device %s\n", deviceName);
+    
     if (pthread_mutex_trylock(&mutex)) {
         throw i2c_error(i2c_error::buildMsg("Failed to acquire bus for device %s, it is already locked", deviceName), __FILE__, __LINE__);
     }
@@ -85,7 +87,7 @@ void I2CBus::acquire(const char * deviceName)
 
     I2CDevice * dev = this->devices[deviceName];
     printf("Got device object for device %s\n", dev->getName());
-    
+
     int err = ioctl(this->_busFd, I2C_WORKER, dev->getAddress());
 
     if (err) {
